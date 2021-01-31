5c05531cc6839fb04d3bce423f1527cf168cbbcc
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2021-01-31 09:22:25 +0000

    add phpmd

--- src/app/User.php
+++ src/app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function ifElsePatter(int $idx, int $idy)
+    {
+        if ($idx == $idy) {
+            return $idx;
+        }
+        return $idy;
+    }
 }
